ef540516340bece23f2798b2aa44dcaa8bbfb2f5
Rayhan <nm.rayhan.1650@gmail.com>
2024-03-23 15:30:31 +0600

    first commit

---
+++ bin/codemama_problemsolving.dart
@@ -0,0 +1,5 @@
+import 'package:codemama_problemsolving/codemama_problemsolving.dart' as codemama_problemsolving;
+
+void main(List<String> arguments) {
+  print('Hello world: ${codemama_problemsolving.calculate()}!');
+}
---
+++ test/codemama_problemsolving_test.dart
@@ -0,0 +1,8 @@
+import 'package:codemama_problemsolving/codemama_problemsolving.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
