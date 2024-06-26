7a256dfb50d86c07a80cebf3110000364c56c8a7
Rayhan <nm.rayhan.1650@gmail.com>
2024-06-26 13:01:17 +0600

    party_dress_code problem solved

--- bin/codemama_problemsolving.dart
+++
@@ -1,5 +0,0 @@
-import 'package:codemama_problemsolving/codemama_problemsolving.dart' as codemama_problemsolving;
-
-void main(List<String> arguments) {
-  print('Hello world: ${codemama_problemsolving.calculate()}!');
-}
