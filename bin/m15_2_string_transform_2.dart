c3d1ad564bf728c6c4387def07f8190f38e6fa4a
Rayhan <nm.rayhan.1650@gmail.com>
2024-06-26 14:50:01 +0600

    string-transform-2 problem solved

--- bin/m15_2_string_transform_2.dart
+++ bin/m15_2_string_transform_2.dart
@@ -2,13 +2,12 @@ import 'dart:io';
 
 void main() {
   String S=stdin.readLineSync()!;
-
   List<String> myList=S.split('');
 
   for(int i=2;i<myList.length;i+=3){
-    myList[i]=myList[i].toUpperCase();
-
+    myList.insert(i,'x');
   }
+
   print(myList.join());
 
 }
